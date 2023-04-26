29dd96ab7c7dfc096ca3ccbb5735be48fa77898e
venuswhispers <venuswhisperer412@gmail.com>
2023-04-26 16:21:01 +0200

    ipld

---
+++ cmd/radiance/car/createcar/ipld/ipldbindcode/schema.go
@@ -0,0 +1,89 @@
+package ipldbindcode
+
+import (
+	_ "embed"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/node/bindnode"
+	"github.com/ipld/go-ipld-prime/schema"
+)
+
+//go:embed ledger.ipldsch
+var embeddedSchema []byte
+
+var Prototypes schemaSlab
+
+type schemaSlab struct {
+	List__Link      schema.TypedPrototype
+	Epoch           schema.TypedPrototype
+	Range           schema.TypedPrototype
+	List__Shredding schema.TypedPrototype
+	Block           schema.TypedPrototype
+	Shredding       schema.TypedPrototype
+	Entry           schema.TypedPrototype
+	Transaction     schema.TypedPrototype
+	Hash            schema.TypedPrototype
+	Buffer          schema.TypedPrototype
+}
+
+func init() {
+	ts, err := ipld.LoadSchemaBytes(embeddedSchema)
+	if err != nil {
+		panic(err)
+	}
+
+	Prototypes.List__Link = bindnode.Prototype(
+		(*List__Link)(nil),
+		ts.TypeByName("List__Link"),
+	)
+
+	Prototypes.Epoch = bindnode.Prototype(
+		(*Epoch)(nil),
+		ts.TypeByName("Epoch"),
+	)
+
+	Prototypes.Range = bindnode.Prototype(
+		(*Range)(nil),
+		ts.TypeByName("Range"),
+	)
+
+	Prototypes.List__Shredding = bindnode.Prototype(
+		(*List__Shredding)(nil),
+		ts.TypeByName("List__Shredding"),
+	)
+
+	Prototypes.Block = bindnode.Prototype(
+		(*Block)(nil),
+		ts.TypeByName("Block"),
+	)
+
+	Prototypes.Shredding = bindnode.Prototype(
+		(*Shredding)(nil),
+		ts.TypeByName("Shredding"),
+	)
+
+	Prototypes.Entry = bindnode.Prototype(
+		(*Entry)(nil),
+		ts.TypeByName("Entry"),
+	)
+
+	Prototypes.Transaction = bindnode.Prototype(
+		(*Transaction)(nil),
+		ts.TypeByName("Transaction"),
+	)
+
+	Prototypes.Hash = bindnode.Prototype(
+		(*Hash)(nil),
+		ts.TypeByName("Hash"),
+	)
+
+	Prototypes.Buffer = bindnode.Prototype(
+		(*Buffer)(nil),
+		ts.TypeByName("Buffer"),
+	)
+}
+
+type (
+	Hash   []byte
+	Buffer []byte
+)
---
+++ cmd/radiance/car/createcar/ipld/ipldbindcode/types.go
@@ -0,0 +1,42 @@
+package ipldbindcode
+
+import "github.com/ipld/go-ipld-prime/datamodel"
+
+type (
+	List__Link []datamodel.Link
+	Epoch      struct {
+		Epoch  int
+		Ranges List__Link
+	}
+)
+
+type Range struct {
+	First  int
+	Last   int
+	Blocks List__Link
+}
+type (
+	List__Shredding []Shredding
+	Block           struct {
+		Kind      int
+		Slot      int
+		Shredding List__Shredding
+		Entries   List__Link
+	}
+)
+
+type Shredding struct {
+	EntryEndIdx int
+	ShredEndIdx int
+}
+type Entry struct {
+	Kind         int
+	NumHashes    int
+	Hash         []uint8
+	Transactions List__Link
+}
+type Transaction struct {
+	Kind     int
+	Data     []uint8
+	Metadata []uint8
+}
