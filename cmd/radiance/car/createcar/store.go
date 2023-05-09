d60e1c7e34a8100b3ebad73f4a5c221f69407dc3
venuswhispers <venuswhisperer412@gmail.com>
2023-05-09 23:50:49 +0200

    Use io_uring for writing blocks to CAR file.

--- cmd/radiance/car/createcar/store.go
+++ cmd/radiance/car/createcar/store.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"sync"
 
+	"github.com/iceber/iouring-go"
 	"go.firedancer.io/radiance/pkg/ipld/car"
 	"k8s.io/klog/v2"
 )
@@ -17,33 +18,80 @@ type carHandle struct {
 	writer     *car.Writer
 	lastOffset int64
 	mu         *sync.Mutex
+
+	ringW *RingedWriter
 }
 
 const (
-	writeBufSize = MiB
+	writeBufSize = MiB * 1
 )
+
+type RingedWriter struct {
+	ring   *iouring.IOURing
+	file   *os.File
+	offset uint64
+	mu     sync.Mutex
+}
+
+func NewRingedWriter(ring *iouring.IOURing, file *os.File) *RingedWriter {
+	return &RingedWriter{
+		ring: ring,
+		file: file,
+	}
+}
+
+func (w *RingedWriter) Write(data []byte) (n int, err error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	compCh := make(chan iouring.Result, 1)
+	_, err = w.ring.Pwrite(w.file, data, w.offset, compCh)
+	if err != nil {
+		return 0, err
+	}
+	w.offset += uint64(len(data))
+	<-compCh
+	return len(data), err
+}
+
+func (w *RingedWriter) Close() error {
+	if w == nil {
+		return nil
+	}
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.ring == nil {
+		return nil
+	}
+	return w.ring.Close()
+}
 
 func (c *carHandle) open(finalCARFilepath string) error {
 	if c.ok() {
 		return fmt.Errorf("handle not closed")
 	}
-	f, err := os.OpenFile(finalCARFilepath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0o666)
+	file, err := os.OpenFile(finalCARFilepath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0o666)
 	if err != nil {
 		return fmt.Errorf("failed to create CAR: %w", err)
 	}
-	cache := bufio.NewWriterSize(f, writeBufSize)
+	iour, err := iouring.New(10)
+	if err != nil {
+		return fmt.Errorf("failed to create iouring: %w", err)
+	}
+	ringWriter := NewRingedWriter(iour, file)
+	cache := bufio.NewWriterSize(ringWriter, writeBufSize)
 	writer, err := car.NewWriter(cache)
 	if err != nil {
 		return fmt.Errorf("failed to start CAR at %s: %w", finalCARFilepath, err)
 	}
 	*c = carHandle{
-		file:       f,
+		file:       file,
 		cache:      cache,
 		writer:     writer,
 		lastOffset: 0,
 		mu:         &sync.Mutex{},
+		ringW:      ringWriter,
 	}
-	klog.Infof("Created new CAR file %s", f.Name())
+	klog.Infof("Created new CAR file %s", file.Name())
 	return nil
 }
 
@@ -57,7 +105,14 @@ func (c *carHandle) close() (err error) {
 	if err = c.cache.Flush(); err != nil {
 		return err
 	}
+	err = c.ringW.Close()
+	if err != nil {
+		return err
+	}
 	err = c.file.Close()
+	if err != nil {
+		return err
+	}
 	*c = carHandle{}
 	return
 }
